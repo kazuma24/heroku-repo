637922b910de9747b947f4bf3a40797edeceebe0
kazuma <kazuma0204kk@gmail.com>
2023-03-26 19:01:36 +0900

    t4

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 import os
+import openai
 
 app = Flask(__name__)
 
 #環境変数取得
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+YOUR_CHANNEL_KEY = os.environ["YOUR_CHAT_GPT_APIKEY"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
@@ -37,16 +39,22 @@ def callback():
 
     return 'OK'
 
-@app.route("/index", methods=['POST'])
-def index():
-    return 'OK'
-
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    openai.api_key = YOUR_CHANNEL_KEY
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "日本語で返答してください"},
+            {"role": "user", "content": event.message.text}
+        ]   
+    )
+    
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=response['choices'][0]['message']['content']))
 
 
 if __name__ == "__main__":
